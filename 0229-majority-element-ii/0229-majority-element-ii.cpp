a1709815af45af766da56f01421bfda18e2cc76e
Tanmoy Choudhury <112960982+tanmoy-OG@users.noreply.github.com>
2024-09-24 12:02:54 +0530

    Time: 7 ms (82.13%), Space: 18.7 MB (56.05%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,23 +1,36 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
+        int cnt1 = 0, cnt2 = 0, el1 = INT_MIN, el2 = INT_MIN;
         vector<int> ans;
-        for(int i = 0; i < nums.size(); i++) {
-            int check = 0;
-            int freq = 0;
-            for(auto it: ans)
-                if(it == nums[i]) {
-                    check = 1;
-                    break;
-                }
-            if(check == 1)
-                continue;
-            for(int j = i; j < nums.size(); j++)
-                if(nums[i] == nums[j])
-                    freq++;
-            if(freq > nums.size()/3)
-                ans.push_back(nums[i]);
+        for (auto it : nums) {
+            if (cnt1 == 0 && el2 != it) {
+                el1 = it;
+                cnt1++;
+            } else if (cnt2 == 0 && el1 != it) {
+                el2 = it;
+                cnt2++;
+            } else if (it == el1)
+                cnt1++;
+            else if (it == el2)
+                cnt2++;
+            else {
+                cnt1--;
+                cnt2--;
+            }
         }
+        cnt1 = 0;
+        cnt2 = 0;
+        for (auto it : nums) {
+            if (it == el1)
+                cnt1++;
+            else if (it == el2)
+                cnt2++;
+        }
+        if (cnt1 > nums.size() / 3)
+            ans.push_back(el1);
+        if (cnt2 > nums.size() / 3)
+            ans.push_back(el2);
         return ans;
     }
 };
