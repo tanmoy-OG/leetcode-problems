7c15f1a583a7ab68b59f89fc1fa647dba4c1aa56
Tanmoy Choudhury <112960982+tanmoy-OG@users.noreply.github.com>
2024-09-25 13:36:45 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -1,22 +1,32 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        int pos = 0, neg = 0;
+        int pos = 0, neg = 0, check = 1;
         vector<int> ans;
-        for (int i = 0; i < nums.size() / 2; i++) {
-            for (int j = pos; j < nums.size(); j++)
-                if (nums[j] >= 0) {
-                    pos = j + 1;
-                    ans.push_back(nums[j]);
-                    break;
-                }
-            for (int k = neg; k < nums.size(); k++)
-                if (nums[k] < 0) {
-                    neg = k + 1;
-                    ans.push_back(nums[k]);
-                    break;
-                }
+        while (pos < nums.size() && neg < nums.size()) {
+            if (check == 1 && nums[pos] >= 0) {
+                ans.push_back(nums[pos]);
+                pos++;
+                check = 0;
+            } else if (check == 1 && nums[pos] < 0)
+                pos++;
+            if (check == 0 && nums[neg] < 0) {
+                ans.push_back(nums[neg]);
+                neg++;
+                check = 1;
+            } else if (check == 0 && nums[neg] >= 0)
+                neg++;
         }
+        // while (pos < nums.size())
+        //     if (nums[pos] >= 0) {
+        //         ans.push_back(nums[pos]);
+        //         pos++;
+        //     }
+        // while (neg < nums.size())
+        //     if (nums[neg] >= 0) {
+        //         ans.push_back(nums[neg]);
+        //         neg++;
+        //     }
         return ans;
     }
 };
