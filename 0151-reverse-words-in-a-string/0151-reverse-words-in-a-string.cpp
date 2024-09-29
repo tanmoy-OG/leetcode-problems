04c508abd3a0070287692338cbfb7d9b44b63620
Tanmoy Choudhury <112960982+tanmoy-OG@users.noreply.github.com>
2024-09-29 20:05:31 +0530

    Time: 9 ms (15.89%), Space: 23.5 MB (15.17%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -2,25 +2,18 @@ class Solution {
 public:
     string reverseWords(string s) {
         string ans, word;
-        int i = 0, j = 0;
         s.erase(0, s.find_first_not_of(" "));
         s.erase(s.find_last_not_of(" ") + 1);
         s += " ";
-        while (i < s.size() && j < s.size()) {
-            if (s[i] == ' ' && i != j) {
-                word += s[j];
-                j++;
-            } else if (s[i] == ' ' && i == j) {
-                if (!word.empty()) {
-                    ans = word + " " + ans;
-                    word = "";
-                }
-                i++;
-                j++;
-            } else
-                i++;
+        for(int i = 0; i < s.size(); i++) {
+            if(s[i] == ' ') {
+                if(s[i-1] == ' ') continue;
+                if(ans == "") ans = word;
+                else ans = word + " " + ans;
+                word = "";
+            }
+            else word+= s[i];
         }
-        ans.erase(ans.find_last_not_of(" ") + 1);
         return ans;
     }
 };
