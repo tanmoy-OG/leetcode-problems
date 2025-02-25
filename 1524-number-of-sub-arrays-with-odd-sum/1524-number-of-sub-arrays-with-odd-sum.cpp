28c623bb335c2b8fb74bd412c3febe87491b14ae
Tanmoy Choudhury <112960982+tanmoy-OG@users.noreply.github.com>
2025-02-25 23:13:25 +0530

    Time: 0 ms (100%), Space: 111.9 MB (96.38%) - LeetHub

---
+++ 1524-number-of-sub-arrays-with-odd-sum/1524-number-of-sub-arrays-with-odd-sum.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int numOfSubarrays(vector<int>& arr) {
+        const int MOD = 1e9 + 7;
+        int oddCount = 0, evenCount = 1;
+        int currSum = 0, result = 0;
+
+        for (int num : arr) {
+            currSum += num;
+
+            if (currSum % 2 == 0) {
+                result = (result + oddCount) % MOD;
+                evenCount++;
+            } else {
+                result = (result + evenCount) % MOD;
+                oddCount++;
+            }
+        }
+
+        return result;
+    }
+};
