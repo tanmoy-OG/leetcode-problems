9aa1835f3309c1faa11b210cba12f68f3eef5636
Tanmoy Choudhury <112960982+tanmoy-OG@users.noreply.github.com>
2025-02-28 00:22:39 +0530

    Time: 0 ms (100%), Space: 45.1 MB (50.7%) - LeetHub

--- 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.cpp
+++ 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.cpp
@@ -1,15 +1,23 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
 class Solution {
 public:
     int maxAbsoluteSum(vector<int>& nums) {
-        int n = nums.size();
-        vector<vector<int>> dp(n + 1, vector<int>(4, 0));
-        for (int i = 1; i <= n; i++) {
-            dp[i][0] = nums[i - 1];
-            dp[i][1] = max(dp[i - 1][1] + nums[i - 1], dp[i][0]);
-            dp[i][2] = min(dp[i - 1][2] + nums[i - 1], dp[i][0]);
-            dp[i][3] = max({abs(dp[i][0]), abs(dp[i][2]), abs(dp[i][1]),
-                            abs(dp[i - 1][3])});
+        int positive = 0, negative = 0, prefixSum = 0, ans = 0;
+
+        for (int n : nums) {
+            prefixSum += n;
+            ans = max(ans, max(abs(prefixSum - negative), abs(prefixSum - positive)));
+
+            if (prefixSum >= 0) {
+                positive = max(positive, prefixSum);
+            } else {
+                negative = min(negative, prefixSum);
+            }
         }
-        return dp[n][3];
+        return ans;
     }
 };
