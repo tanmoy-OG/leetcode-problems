ac52423a3d6f3f3cdf9ccea3e1ef38c919e85282
Tanmoy Choudhury <112960982+tanmoy-OG@users.noreply.github.com>
2026-09-04 18:26:58 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (52.65%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
@@ -6,24 +6,24 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     void flatten(TreeNode* root) {
-        if (!root)
-            return;
-        stack<TreeNode*> st;
-        st.push(root);
-        while(!st.empty()) {
-            TreeNode* node = st.top();
-            st.pop();
-            if(node->right) st.push(node->right);
-            if(node->left) st.push(node->left);
-            node->left = NULL;
-            if(!st.empty())
-                node->right = st.top();
+        TreeNode* node = root;
+        while (node) {
+            if (node->left) {
+                TreeNode* temp = node->left;
+                while (temp->right)
+                    temp = temp->right;
+                temp->right = node->right;
+                node->right = node->left;
+                node->left = NULL;
+            }
+            node = node->right;
         }
     }
 };
