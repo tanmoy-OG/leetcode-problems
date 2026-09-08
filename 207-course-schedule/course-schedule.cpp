1d1c83803e8a9263219de3e3a38f9d93d8fcbe6a
Tanmoy Choudhury <112960982+tanmoy-OG@users.noreply.github.com>
2026-09-08 19:41:53 +0530

    Time: 4 ms (64.64%) | Memory: 20.3 MB (16.98%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -5,7 +5,7 @@ public:
         vector<int> vis(numCourses, 0);
         vector<int> pathVis(numCourses, 0);
         for(auto it: prerequisites)
-            adj[it[0]].push_back(it[1]);
+            adj[it[1]].push_back(it[0]);
         for(int i = 0; i < numCourses; i++)
             if(!vis[i])
                 if(dfs(i, adj, vis, pathVis))
