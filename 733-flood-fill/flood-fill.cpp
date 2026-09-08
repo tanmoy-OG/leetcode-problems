4e06a2686541df0507c939abdbdaf18f56e61f23
Tanmoy Choudhury <112960982+tanmoy-OG@users.noreply.github.com>
2026-09-08 16:12:17 +0530

    Time: 3 ms (12.57%) | Memory: 18.1 MB (56.24%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -7,28 +7,20 @@ public:
             return image;
         int m = image.size(), n = image[0].size();
         vector<vector<int>> v = image;
-        queue<pair<pair<int, int>, int>> q;
-        v[sr][sc] = color;
-        q.push({{sr, sc}, color});
-        while (!q.empty()) {
-            int row = q.front().first.first;
-            int col = q.front().first.second;
-            int currColor = q.front().second;
-            q.pop();
-            if (currColor == color) {
-                int dRow[] = {-1, 0, 1, 0};
-                int dCol[] = {0, 1, 0, -1};
-                for (int i = 0; i < 4; i++) {
-                    int nRow = row + dRow[i];
-                    int nCol = col + dCol[i];
-                    if (nRow >= 0 && nRow < m && nCol >= 0 && nCol < n &&
-                        v[nRow][nCol] == check) {
-                        q.push({{nRow, nCol}, color});
-                        v[nRow][nCol] = color;
-                    }
-                }
-            }
-        }
+        dfs(v, m, n, sr, sc, color, check);
         return v;
+    }
+    void dfs(vector<vector<int>>& v, int& m, int& n, int row, int col, int& color, int& check) {
+        if(v[row][col] != check)
+            return;
+        v[row][col] = color;
+        int dRow[] = {-1, 0, 1, 0};
+        int dCol[] = {0, 1, 0, -1};
+        for(int i = 0; i < 4; i++) {
+            int nRow = row + dRow[i];
+            int nCol = col + dCol[i];
+            if(nRow >= 0 && nRow < m && nCol >= 0 && nCol < n)
+                dfs(v, m, n, nRow, nCol, color, check);
+        }
     }
 };
