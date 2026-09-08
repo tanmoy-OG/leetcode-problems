3bf4c66795bd77ec42ade828c192c0866fb01b33
Tanmoy Choudhury <112960982+tanmoy-OG@users.noreply.github.com>
2026-09-08 14:18:43 +0530

    Time: 0 ms (100.00%) | Memory: 18.4 MB (30.55%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -2,12 +2,13 @@ class Solution {
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc,
                                   int color) {
-        int m = image.size(), n = image[0].size(), check = image[sr][sc];
+        int check = image[sr][sc];
+        if (check == color)
+            return image;
+        int m = image.size(), n = image[0].size();
         vector<vector<int>> v = image;
-        vector<vector<int>> vis(m, vector<int>(n, 0));
         queue<pair<pair<int, int>, int>> q;
         v[sr][sc] = color;
-        vis[sr][sc] = 1;
         q.push({{sr, sc}, color});
         while (!q.empty()) {
             int row = q.front().first.first;
@@ -21,12 +22,9 @@ public:
                     int nRow = row + dRow[i];
                     int nCol = col + dCol[i];
                     if (nRow >= 0 && nRow < m && nCol >= 0 && nCol < n &&
-                        !vis[nRow][nCol]) {
-                        if (v[nRow][nCol] == check) {
-                            q.push({{nRow, nCol}, color});
-                            v[nRow][nCol] = color;
-                        }
-                        vis[nRow][nCol] = 1;
+                        v[nRow][nCol] == check) {
+                        q.push({{nRow, nCol}, color});
+                        v[nRow][nCol] = color;
                     }
                 }
             }
